f65849a4a60364592971ed4890a820a95303759d
Gaku Nakagawa <gaku@sleeek.io>
2020-03-05 15:51:36 +0900

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace csharp_helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Sider World!");
         }
     }
 }
